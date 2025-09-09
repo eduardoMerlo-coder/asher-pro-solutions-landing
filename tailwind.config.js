ec97b82294dd82f11bb9c804c3098f35de0a2d5d
eduardo <agredu1941@gmail.com>
2025-09-08 19:13:08 -0500

    firts commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        sans: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        black: "900",
+      },
+    },
+  },
+  plugins: [],
+};
