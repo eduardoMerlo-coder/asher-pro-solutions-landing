0c2b549be78b7dbf8ce7cf81e97c2627caee5b12
Eduardo Jaucha Merlo <eduardo.jmerlo@gmail.com>
2025-09-10 22:25:32 -0500

    add database

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://asherpro-solutions.com", // URL de producción
+  output: "static", // o 'hybrid' si necesitas SSR
+  compressHTML: true,
+  build: {
+    assets: "assets",
+    inlineStylesheets: "auto",
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            swiper: ["swiper"],
+          },
+        },
+      },
+    },
   },
 });
